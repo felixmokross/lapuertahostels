1f12b819cb98b94bec103b12e73810b8fd7b8023
Felix Mokross <felixmokross@icloud.com>
2023-12-28 15:18:39 -0500

    WIP design

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,7 +6,7 @@ import { loader } from "~/root";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "La Puerta Hostels" },
+    { title: "LA PUERTA HOSTELS" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
@@ -105,16 +105,18 @@ export default function Index() {
             yourself in the river of{" "}
             <strong className="text-neutral-900">Minca</strong>. Our variety of
             heartful accommodations in the city of Santa Marta are{" "}
-            <strong className="text-neutral-900">your perfect homebase.</strong>
+            <strong className="text-neutral-900">
+              your perfect home base.
+            </strong>
           </p>
         </div>
 
         <div className="relative mt-24">
-          <div className="absolute inset-0 -z-10 h-2/3 bg-gradient-to-br from-puerta-400 to-puerta-600"></div>
+          <div className="absolute inset-0 -z-10 h-[23rem] bg-gradient-to-br from-puerta-400 to-puerta-600"></div>
           <div className="py-16">
             <div className="mx-auto max-w-4xl">
               <h2 className="font-serif text-5xl tracking-tight text-white">
-                Your Homebase in Santa Marta
+                Your Home Base for a Perfect Trip
               </h2>
               <p className="mt-6 hyphens-auto text-justify text-xl leading-relaxed text-white">
                 Choose from our three accommodation offerings in Santa Marta.
@@ -151,9 +153,45 @@ export default function Index() {
             </div>
           </div>
         </div>
-        <div>
-          <h3>Do you know Santa Marta?</h3>
-
+        <div className="relative mx-auto mt-8 max-w-4xl">
+          <div className="h-[32rem] overflow-hidden rounded-lg shadow-md">
+            <img
+              src={`${imagekitBaseUrl}/oscar-ivan-esquivel-arteaga-DZVY-1I2peQ-unsplash.jpg?updatedAt=1703778785707&tr=w-1000,ar-16-9,c-maintain_ratio,fo-auto`}
+              alt="View of Santa Marta"
+              className="h-full w-full object-cover"
+            />
+            <div className="absolute inset-0 flex rounded-lg bg-gradient-to-t from-transparent to-black/40 px-8 py-4">
+              <h3
+                className="font-serif text-6xl leading-relaxed tracking-tight text-white"
+                style={{ textShadow: "0 0 50px black" }}
+              >
+                Do You Know{" "}
+                <span className="text-puerta-200">Santa Marta?</span>
+              </h3>
+            </div>
+          </div>
+          <div className="absolute inset-0 flex items-end justify-end">
+            <div className="max-w-lg translate-x-12 translate-y-20 space-y-4 rounded-md bg-gradient-to-bl from-puerta-100 to-puerta-300 px-6 py-4 shadow-lg">
+              <p className="hyphens-auto text-justify text-base leading-relaxed text-puerta-800">
+                Santa Marta, nestled{" "}
+                <strong>
+                  between the Caribbean Sea and the Sierra Nevada mountains,
+                </strong>{" "}
+                beckons tourists with its captivating blend of{" "}
+                <strong>natural beauty</strong> and{" "}
+                <strong>rich cultural heritage</strong>. Boasting pristine
+                beaches, lush national parks, and a historic city center, Santa
+                Marta offers an <strong>enchanting escape</strong> for travelers
+                seeking a perfect balance of{" "}
+                <strong>sun-soaked relaxation</strong> and{" "}
+                <strong>
+                  exploration of Colombia&rsquo;s diverse landscapes.
+                </strong>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="mx-auto mt-24 max-w-4xl">
           <p>Some text about Santa Marta</p>
         </div>
 
@@ -205,20 +243,21 @@ function AccommodationCard({
     >
       <div className="relative aspect-[16/9] bg-white">
         <img
-          src={`${imagekitBaseUrl}/${image.src}&tr=ar-16-9,w-600`}
+          src={`${imagekitBaseUrl}/${image.src}&tr=ar-16-9,w-600,fo-custom`}
+          // fo-custom is not needed on all images
           alt=""
           className="h-full w-full object-cover group-hover:opacity-75"
         />
       </div>
       <div
-        className={cn("space-y-1 px-6 py-4 text-base text-white", {
+        className={cn("space-y-2 px-6 py-4 text-base text-white", {
           "group-hover:text-azul-800": color === "azul",
           "group-hover:text-aqua-800": color === "aqua",
           "group-hover:text-apartments-800": color === "appartments",
         })}
       >
         <h4 className="text-sm font-bold uppercase tracking-wider">{name}</h4>
-        <p className="">{description}</p>
+        <p className="leading-relaxed">{description}</p>
       </div>
     </Link>
   );
