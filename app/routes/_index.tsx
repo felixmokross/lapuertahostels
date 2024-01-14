c6b6568199ce0b27a79981ba064411e2c36c0230
Felix Mokross <felix.mokross@zuehlke.com>
2024-01-14 16:17:42 +0100

    Add footer, small allover improvements

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,13 +4,62 @@ import { Carousel } from "~/components/carousel";
 import { cn } from "~/components/classnames";
 import { loader } from "~/root";
 import { XMarkIcon } from "@heroicons/react/20/solid";
-import { useState } from "react";
+import { SVGProps, useState } from "react";
+import { JSX } from "react/jsx-runtime";
 
 export const meta: MetaFunction = () => {
   return [
     { title: "LA PUERTA HOSTELS" },
     { name: "description", content: "Welcome to Remix!" },
   ];
+};
+
+const footerNavigation = {
+  hotel: [
+    { name: "About Us", href: "#" },
+    { name: "Puerta Aqua", href: "#" },
+    { name: "La Puerta Azul", href: "#" },
+  ],
+  experiences: [
+    { name: "Santa Marta", href: "#" },
+    { name: "Lost City", href: "#" },
+    { name: "Tayrona Park", href: "#" },
+    { name: "Minca", href: "#" },
+  ],
+  legal: [
+    { name: "Terms", href: "#" },
+    { name: "Cancelation", href: "#" },
+    { name: "Privacy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+  social: [
+    {
+      name: "Facebook",
+      href: "#",
+      icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+          <path
+            fillRule="evenodd"
+            d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
+    },
+    {
+      name: "Instagram",
+      href: "https://www.instagram.com/lapuertahostels",
+      icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+          <path
+            fillRule="evenodd"
+            d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
+    },
+  ],
 };
 
 export default function Index() {
@@ -134,8 +183,8 @@ export default function Index() {
           </p>
         </div>
 
-        <div className="relative mt-24">
-          <div className="absolute inset-0 -z-10 h-[23rem] bg-gradient-to-br from-puerta-400 to-puerta-600"></div>
+        <div className="relative mt-36">
+          <div className="absolute inset-0 -z-10 h-[23rem] bg-gradient-to-br from-puerta-700 to-puerta-600"></div>
           <div className="py-16">
             <div className="mx-auto max-w-4xl">
               <h2 className="font-serif text-5xl tracking-tight text-white">
@@ -157,18 +206,18 @@ export default function Index() {
                 description="Stay at our lively hostel in the heart of Santa Marta and meet travelers from all over the world. Our rooftop bar is perfect for a get-together at night."
               />
               <AccommodationCard
-                name="Puerta Azul"
+                name="La Puerta Azul"
                 color="azul"
                 image={{
                   src: "358685842_17937739007690648_2983057103105632929_n.jpg?updatedAt=1703702151179",
-                  alt: "Puerta Azul",
+                  alt: "La Puerta Azul",
                 }}
-                description="Being one of the oldest houses in Santa Marta, Puerta Aqua is filled with beauty and history. It can also be booked completely as a private six-room villa."
+                description="Being one of the oldest houses in Santa Marta, La Puerta Aqua is filled with beauty and history. It can also be booked completely as a private six-room villa."
               />
             </div>
           </div>
         </div>
-        <div className="relative mx-auto mt-8 max-w-4xl">
+        <div className="relative mx-auto mt-32 max-w-4xl">
           <div className="h-[32rem] overflow-hidden rounded-lg shadow-md">
             <img
               src={`${imagekitBaseUrl}/oscar-ivan-esquivel-arteaga-DZVY-1I2peQ-unsplash.jpg?updatedAt=1703778785707&tr=w-1000,ar-16-9,c-maintain_ratio,fo-auto`}
@@ -207,10 +256,10 @@ export default function Index() {
           </div>
         </div>
 
-        <div className="mx-auto mb-20 mt-60 grid max-w-4xl grid-cols-2 items-center justify-center gap-16">
+        <div className="mx-auto mb-20 mt-72 grid max-w-4xl grid-cols-2 items-center justify-center gap-16">
           <div className="-ml-10 mr-12 mt-4 aspect-[3/4] -rotate-6 overflow-hidden rounded-md shadow-lg">
             <img
-              src={`${imagekitBaseUrl}/366944756_17942281163690648_3066160991932660286_n.jpg?updatedAt=1704147703325&tr=ar-3-4,w-1600,e-grayscale`}
+              src={`${imagekitBaseUrl}/351429301_1381427532589680_2319248312954498147_n.jpg?updatedAt=1703702171449&tr=ar-3-4,w-1600,e-grayscale`}
               alt=""
               className="h-full w-full object-cover"
             />
@@ -255,8 +304,98 @@ export default function Index() {
           </div>
         </div>
       </main>
-      <footer>
-        <div>Footer</div>
+      <footer className="mt-40 bg-puerta-100" aria-labelledby="footer-heading">
+        <h2 id="footer-heading" className="sr-only">
+          Footer
+        </h2>
+        <div className="mx-auto max-w-5xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
+          <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+            <div className="space-y-8">
+              <h6 className="mt-2 flex items-center gap-2 font-serif text-base uppercase tracking-wider text-neutral-900">
+                <img
+                  className="h-7"
+                  src={`${imagekitBaseUrl}/logos/logo-puerta-simple.png?updatedAt=1703906701749&tr=h-56`}
+                  alt="La Puerta Hostels Logo"
+                />
+                La Puerta Hostels
+              </h6>
+              <p className="text-sm leading-6 text-neutral-600">
+                Welcome to the door of the dreams. Dreams come true if you
+                reserve well!
+              </p>
+              <div className="flex space-x-6">
+                {footerNavigation.social.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="text-neutral-400 hover:text-neutral-500"
+                  >
+                    <span className="sr-only">{item.name}</span>
+                    <item.icon className="h-6 w-6" aria-hidden="true" />
+                  </a>
+                ))}
+              </div>
+            </div>
+            <div className="mt-16 grid grid-cols-3 gap-8 xl:col-span-2 xl:mt-0">
+              <div>
+                <h3 className="text-sm font-semibold leading-6 text-neutral-900">
+                  Hotel
+                </h3>
+                <ul className="mt-6 space-y-4">
+                  {footerNavigation.hotel.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-sm leading-6 text-neutral-600 hover:text-neutral-900"
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="mt-10 md:mt-0">
+                <h3 className="text-sm font-semibold leading-6 text-neutral-900">
+                  Experiences
+                </h3>
+                <ul className="mt-6 space-y-4">
+                  {footerNavigation.experiences.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-sm leading-6 text-neutral-600 hover:text-neutral-900"
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="mt-10 md:mt-0">
+                <h3 className="text-sm font-semibold leading-6 text-neutral-900">
+                  Legal
+                </h3>
+                <ul className="mt-6 space-y-4">
+                  {footerNavigation.legal.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-sm leading-6 text-neutral-600 hover:text-neutral-900"
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="mt-16 border-t border-neutral-900/10 pt-8 sm:mt-20 lg:mt-24">
+            <p className="text-xs leading-5 text-neutral-500">
+              &copy; 2024 LA PUERTA HOSTELS, Inc. All rights reserved.
+            </p>
+          </div>
+        </div>
       </footer>
     </>
   );
