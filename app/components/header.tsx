031eb8d2aff9f986c9fb7554ef82aa6f901f1c15
Felix Mokross <felix.mokross@zuehlke.com>
2024-01-27 19:44:23 +0100

    Extract header

---
+++ app/components/header.tsx
@@ -0,0 +1,96 @@
+import { GlobeAmericasIcon } from "@heroicons/react/20/solid";
+import { Link, useRouteLoaderData } from "@remix-run/react";
+import { cn } from "./classnames";
+import { useTranslation } from "react-i18next";
+import { loader as rootLoader } from "~/root";
+
+export function Header() {
+  const rootLoaderData = useRouteLoaderData<typeof rootLoader>("root");
+  if (!rootLoaderData) throw new Error("root loader not found");
+
+  const { t, i18n } = useTranslation();
+  const { imagekitBaseUrl } = rootLoaderData;
+  return (
+    <header className="grid grid-cols-3 items-center px-4 py-4">
+      <Link to="/">
+        <h1 className="flex items-center gap-4 font-serif text-2xl uppercase tracking-wide text-neutral-900">
+          <img
+            src={`${imagekitBaseUrl}/logos/logo-puerta-simple.png?updatedAt=1703906701749&tr=h-80`}
+            alt="La Puerta Hostels Logo"
+            width={33}
+            height={40}
+          />
+          <>La Puerta Hostels</>
+        </h1>
+      </Link>
+      <div className="space-x-10 justify-self-center text-nowrap text-sm font-bold text-neutral-500">
+        <Link to="" className={cn("hover:text-neutral-900")}>
+          Puerta Aqua
+        </Link>
+        <Link to="" className={cn("hover:text-neutral-900")}>
+          La Puerta Azul
+        </Link>
+        <Link to="#santa-marta" className={cn("hover:text-neutral-900")}>
+          Santa Marta
+        </Link>
+        <Link to="#about-us" className={cn("hover:text-neutral-900")}>
+          {t("aboutUs")}
+        </Link>
+        <Link to="" className={cn("hover:text-neutral-900")}>
+          {t("contact")}
+        </Link>
+      </div>
+      <div className="group flex items-center justify-end gap-2 text-sm font-bold text-neutral-500">
+        <GlobeAmericasIcon className="h-4" />
+        <Link
+          to="/en"
+          reloadDocument
+          className={cn(
+            "hover:text-neutral-900",
+            i18n.language === "en"
+              ? "text-neutral-900"
+              : "hidden group-hover:inline",
+          )}
+        >
+          English
+        </Link>
+        <Link
+          to="/es"
+          reloadDocument
+          className={cn(
+            "hover:text-neutral-900",
+            i18n.language === "es"
+              ? "text-neutral-900"
+              : "hidden group-hover:inline",
+          )}
+        >
+          Español
+        </Link>
+        <Link
+          to="/de"
+          reloadDocument
+          className={cn(
+            "hover:text-neutral-900",
+            i18n.language === "de"
+              ? "text-neutral-900"
+              : "hidden group-hover:inline",
+          )}
+        >
+          Deutsch
+        </Link>
+        <Link
+          to="/fr"
+          reloadDocument
+          className={cn(
+            "hover:text-neutral-900",
+            i18n.language === "fr"
+              ? "text-neutral-900"
+              : "hidden group-hover:inline",
+          )}
+        >
+          Français
+        </Link>
+      </div>
+    </header>
+  );
+}
--- app/routes/$(locale)._index.tsx
+++ app/routes/$(locale)._index.tsx
@@ -2,12 +2,12 @@ import type { MetaFunction } from "@remix-run/node";
 import { Link, useRouteLoaderData } from "@remix-run/react";
 import { Carousel } from "~/components/carousel";
 import { cn } from "~/components/classnames";
-import { GlobeAmericasIcon } from "@heroicons/react/20/solid";
 import { SVGProps } from "react";
 import { JSX } from "react/jsx-runtime";
 import { loader as rootLoader } from "~/root";
 import { useTranslation } from "react-i18next";
 import { Banner } from "~/components/banner";
+import { Header } from "~/components/header";
 
 export const meta: MetaFunction = () => {
   return [
@@ -69,94 +69,14 @@ export default function Index() {
   const rootLoaderData = useRouteLoaderData<typeof rootLoader>("root");
   if (!rootLoaderData) throw new Error("root loader not found");
 
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
   const { imagekitBaseUrl } = rootLoaderData;
   return (
     <>
       <Banner cta={t("bannerCta")} ctaTo="/">
         {t("bannerMessage")}
       </Banner>
-      <header className="grid grid-cols-3 items-center px-4 py-4">
-        <Link to="/">
-          <h1 className="flex items-center gap-4 font-serif text-2xl uppercase tracking-wide text-neutral-900">
-            <img
-              src={`${imagekitBaseUrl}/logos/logo-puerta-simple.png?updatedAt=1703906701749&tr=h-80`}
-              alt="La Puerta Hostels Logo"
-              width={33}
-              height={40}
-            />
-            <>La Puerta Hostels</>
-          </h1>
-        </Link>
-        <div className="space-x-10 justify-self-center text-nowrap text-sm font-bold text-neutral-500">
-          <Link to="" className={cn("hover:text-neutral-900")}>
-            Puerta Aqua
-          </Link>
-          <Link to="" className={cn("hover:text-neutral-900")}>
-            La Puerta Azul
-          </Link>
-          <Link to="#santa-marta" className={cn("hover:text-neutral-900")}>
-            Santa Marta
-          </Link>
-          <Link to="#about-us" className={cn("hover:text-neutral-900")}>
-            {t("aboutUs")}
-          </Link>
-          <Link to="" className={cn("hover:text-neutral-900")}>
-            {t("contact")}
-          </Link>
-        </div>
-        <div className="group flex items-center justify-end gap-2 text-sm font-bold text-neutral-500">
-          <GlobeAmericasIcon className="h-4" />
-          <Link
-            to="/en"
-            reloadDocument
-            className={cn(
-              "hover:text-neutral-900",
-              i18n.language === "en"
-                ? "text-neutral-900"
-                : "hidden group-hover:inline",
-            )}
-          >
-            English
-          </Link>
-          <Link
-            to="/es"
-            reloadDocument
-            className={cn(
-              "hover:text-neutral-900",
-              i18n.language === "es"
-                ? "text-neutral-900"
-                : "hidden group-hover:inline",
-            )}
-          >
-            Español
-          </Link>
-          <Link
-            to="/de"
-            reloadDocument
-            className={cn(
-              "hover:text-neutral-900",
-              i18n.language === "de"
-                ? "text-neutral-900"
-                : "hidden group-hover:inline",
-            )}
-          >
-            Deutsch
-          </Link>
-          <Link
-            to="/fr"
-            reloadDocument
-            className={cn(
-              "hover:text-neutral-900",
-              i18n.language === "fr"
-                ? "text-neutral-900"
-                : "hidden group-hover:inline",
-            )}
-          >
-            Français
-          </Link>
-        </div>
-      </header>
+      <Header />
       <main>
         <Carousel
           items={[
