65311812e0a4ed11d38e8b78746eb67ab82e7f1d
Felix Mokross <felix.mokross@zuehlke.com>
2024-01-27 16:33:55 +0100

    Fix storybook i18next integration

--- .storybook/i18next.ts
+++ .storybook/i18next.ts
@@ -1,33 +1,38 @@
 import { initReactI18next } from "react-i18next";
-import i18n from "i18next";
+import i18n, { Resource } from "i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
 const ns = ["common"];
 const supportedLngs = ["en", "es"];
 
-const resources = {};
-for (const lng of supportedLngs) {
-  resources[lng] = {};
-  for (const n of ns) {
-    const data = await import(`../public/locales/${lng}/${n}.json`);
-    resources[lng][n] = data;
+async function loadResources() {
+  const resources = {};
+  for (const lng of supportedLngs) {
+    resources[lng] = {};
+    for (const n of ns) {
+      const data = await import(`../public/locales/${lng}/${n}.json`);
+      resources[lng][n] = data;
+    }
   }
+  return resources as Resource | undefined;
 }
 
-i18n
-  .use(initReactI18next)
-  .use(LanguageDetector)
-  .use(Backend)
-  .init({
-    lng: "en",
-    fallbackLng: "en",
-    defaultNS: "common",
-    ns,
-    interpolation: { escapeValue: false },
-    react: { useSuspense: false },
-    supportedLngs,
-    resources,
-  });
+loadResources().then((resources) =>
+  i18n
+    .use(initReactI18next)
+    .use(LanguageDetector)
+    .use(Backend)
+    .init({
+      lng: "en",
+      fallbackLng: "en",
+      defaultNS: "common",
+      ns,
+      interpolation: { escapeValue: false },
+      react: { useSuspense: false },
+      supportedLngs,
+      resources,
+    }),
+);
 
 export default i18n;
