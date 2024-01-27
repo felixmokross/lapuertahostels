f6a688a5cabdf05dd8b6ea1204584ca11b9e56a3
Felix Mokross <felix.mokross@zuehlke.com>
2024-01-27 20:12:50 +0100

    Extract footer

---
+++ app/components/footer.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Footer } from "./footer";
+
+const meta = {
+  title: "Footer",
+  component: Footer,
+  argTypes: {},
+} satisfies Meta<typeof Footer>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
