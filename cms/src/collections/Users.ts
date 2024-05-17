c1913562742e36de54a2c911aea558db54f9ba11
Felix Mokross <felixmokross@gmail.com>
2024-05-18 01:23:33 +0200

    Work further on CMS data

--- cms/src/collections/Users.ts
+++ cms/src/collections/Users.ts
@@ -2,6 +2,16 @@ import { CollectionConfig } from "payload/types";
 
 export const Users: CollectionConfig = {
   slug: "users",
+  labels: {
+    singular: {
+      en: "User",
+      es: "Usuario",
+    },
+    plural: {
+      en: "Users",
+      es: "Usuarios",
+    },
+  },
   auth: true,
   admin: {
     useAsTitle: "email",
--- cms/src/globals/Common.ts
+++ cms/src/globals/Common.ts
@@ -2,39 +2,71 @@ import { GlobalConfig } from "payload/types";
 
 export const Common: GlobalConfig = {
   slug: "common",
+  label: {
+    en: "Common",
+    es: "Común",
+  },
   access: { read: () => true },
   fields: [
     {
       name: "banner",
+      label: {
+        en: "Banner",
+        es: "Banner",
+      },
       type: "group",
       fields: [
         {
           name: "message",
+          label: {
+            en: "Message",
+            es: "Mensaje",
+          },
           localized: true,
           type: "text",
         },
         {
           name: "cta",
+          label: {
+            en: "CTA",
+            es: "CTA",
+          },
           localized: true,
           type: "text",
         },
         {
           name: "ctaUrl",
+          label: {
+            en: "CTA URL",
+            es: "URL del CTA",
+          },
           type: "text",
         },
       ],
     },
     {
       name: "footer",
+      label: {
+        en: "Footer",
+        es: "Pie de Página",
+      },
       type: "group",
       fields: [
         {
           name: "address",
+          label: {
+            en: "Address",
+            es: "Dirección",
+          },
           type: "textarea",
           required: true,
         },
         {
           name: "copyright",
+          label: {
+            en: "Copyright",
+            es: "Derechos de Autor",
+          },
           type: "text",
           required: true,
           localized: true,
