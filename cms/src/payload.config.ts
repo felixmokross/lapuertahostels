8304d5ab2c77c4818ec64ce1cc1732e92ac65ce4
Felix Mokross <felixmokross@gmail.com>
2024-05-18 10:07:44 +0200

    Avoid requiring live preview URL in build

--- cms/src/payload.config.ts
+++ cms/src/payload.config.ts
@@ -10,21 +10,25 @@ import { Users } from "./collections/Users";
 import { Home } from "./globals/Home";
 import { Common } from "./globals/Common";
 
-const livePreviewUrl = process.env.PAYLOAD_PUBLIC_LIVE_PREVIEW_URL;
-if (!livePreviewUrl) {
-  throw new Error(
-    "Missing PAYLOAD_PUBLIC_LIVE_PREVIEW_URL in environment variables",
-  );
-}
+function getLivePreviewUrl() {
+  const livePreviewUrl = process.env.PAYLOAD_PUBLIC_LIVE_PREVIEW_URL;
+  if (!livePreviewUrl) {
+    throw new Error(
+      "Missing PAYLOAD_PUBLIC_LIVE_PREVIEW_URL in environment variables",
+    );
+  }
 
-console.info(`Live Preview URL is ${livePreviewUrl}`);
+  console.info(`Live Preview URL is ${livePreviewUrl}`);
+
+  return livePreviewUrl;
+}
 
 export default buildConfig({
   admin: {
     user: Users.slug,
     bundler: webpackBundler(),
     livePreview: {
-      url: ({ locale }) => `${livePreviewUrl}?lng=${locale}`,
+      url: ({ locale }) => `${getLivePreviewUrl()}?lng=${locale}`,
       globals: [Home.slug, Common.slug],
     },
     meta: {
