d26cdde3b74f649ff083de03edd7b8a988cb0581
Felix Mokross <felixmokross@gmail.com>
2024-05-24 22:36:17 +0200

    List fly instances

--- frontend/app/routes/version.tsx
+++ frontend/app/routes/version.tsx
@@ -1,6 +1,16 @@
 import { json } from "@remix-run/node";
 import packageJson from "../../package.json";
+import { resolve6 } from "dns/promises";
 
-export function loader() {
-  return json({ version: packageJson.version });
+async function getFlyInstances(): Promise<string[]> {
+  const address = `global.${process.env.FLY_APP_NAME}.internal`;
+  const ipv6s = await resolve6(address);
+  return ipv6s.map((ip) => `http://[${ip}]:8080`);
+}
+
+export async function loader() {
+  return json({
+    version: packageJson.version,
+    instances: await getFlyInstances(),
+  });
 }
