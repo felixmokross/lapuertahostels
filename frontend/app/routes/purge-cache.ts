637cb938df1a62a9a4269291558f677e29a72353
Felix Mokross <felixmokross@gmail.com>
2024-05-25 11:43:39 +0200

    Work on cache purging

--- frontend/app/routes/purge-cache.ts
+++ frontend/app/routes/purge-cache.ts
@@ -2,8 +2,8 @@ import { ActionFunctionArgs } from "@remix-run/node";
 import { purgeCacheFor } from "~/common/cms-data";
 
 export async function action({ request }: ActionFunctionArgs) {
-  const form = await request.formData();
-  const url = form.get("url") as string;
+  const { url } = await request.json();
+  console.log(`Purging cache for ${url}`);
   await purgeCacheFor(url);
 
   return new Response(null, { status: 204 });
