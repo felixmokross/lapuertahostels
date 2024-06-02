c0a1b6b1468a4eee252dd32d0c9b4d8207a844d7
Felix Mokross <felixmokross@gmail.com>
2024-06-02 18:22:18 +0200

    Allow to configure CTA variant in CMS

--- cms/src/fields/call-to-action.ts
+++ cms/src/fields/call-to-action.ts
@@ -1,14 +1,16 @@
-import { GroupField } from "payload/types";
+import { GroupField, RadioField } from "payload/types";
 import { showField } from "./show";
 
 export type CallToActionFieldOptions = {
   optional?: boolean;
   showByDefault?: boolean;
+  variant?: { default: "primary" | "secondary" } | false;
 };
 
 export function makeCallToActionField({
   optional = false,
   showByDefault = true,
+  variant = { default: "secondary" },
 }: CallToActionFieldOptions = {}): GroupField {
   const condition = optional ? (_, siblingData) => siblingData.show : undefined;
   return {
@@ -41,6 +43,35 @@ export function makeCallToActionField({
         required: true,
         admin: { condition },
       },
+      ...(variant
+        ? [
+            {
+              name: "variant",
+              type: "radio",
+              label: {
+                en: "Variant",
+                es: "Variante",
+              },
+              options: [
+                {
+                  label: {
+                    en: "Primary",
+                    es: "Primario",
+                  },
+                  value: "primary",
+                },
+                {
+                  label: {
+                    en: "Secondary",
+                    es: "Secundario",
+                  },
+                  value: "secondary",
+                },
+              ],
+              defaultValue: variant.default,
+            } as RadioField,
+          ]
+        : []),
     ],
   };
 }
