4811cb0260c90e15ab75b88b2ddb396e93be535e
Felix Mokross <felixmokross@gmail.com>
2024-06-02 19:05:07 +0200

    Simplify Accommodation Selector block component

--- frontend/app/blocks/layout-blocks.tsx
+++ frontend/app/blocks/layout-blocks.tsx
@@ -1,4 +1,4 @@
-import { Brand, Page } from "~/payload-types";
+import { Page } from "~/payload-types";
 import { AccommodationSelectorBlock } from "./accommodation-selector-block";
 import { ImageWithFloatingTextBlock } from "./image-with-floating-text-block";
 import { LeadBlock } from "./lead-block";
@@ -15,20 +15,7 @@ export function LayoutBlocks({ data }: LayoutBlocksProps) {
       case "Lead":
         return <LeadBlock key={block.id} {...block} />;
       case "AccommodationSelector":
-        return (
-          <AccommodationSelectorBlock
-            key={block.id}
-            id={block.elementId || undefined}
-            heading={block.heading}
-            text={block.text}
-            accommodationCards={block.cards.map((card) => ({
-              brand: card.brand as Brand,
-              image: { src: card.image.url, alt: card.image.alt },
-              description: card.description,
-              id: card.id!,
-            }))}
-          />
-        );
+        return <AccommodationSelectorBlock key={block.id} {...block} />;
       case "ImageWithFloatingText":
         return <ImageWithFloatingTextBlock key={block.id} {...block} />;
       case "Story":
