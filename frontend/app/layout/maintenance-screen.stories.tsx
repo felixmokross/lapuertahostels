0dd4e7bb2ade1d9d635e2f8b1b2c89bd6be404e2
Felix Mokross <felixmokross@gmail.com>
2024-06-12 22:13:25 +0200

    Remove locale modes from Maintenance Screen

--- frontend/app/layout/maintenance-screen.stories.tsx
+++ frontend/app/layout/maintenance-screen.stories.tsx
@@ -1,21 +1,11 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { MaintenanceScreen } from "./maintenance-screen";
-import { allModes } from ".storybook/modes";
 
 const meta = {
   title: "layout/Maintenance Screen",
   component: MaintenanceScreen,
   argTypes: {},
-  parameters: {
-    chromatic: {
-      modes: {
-        "locale-es": allModes["locale-es"],
-        "locale-de": allModes["locale-de"],
-        "locale-fr": allModes["locale-fr"],
-      },
-    },
-  },
 } satisfies Meta<typeof MaintenanceScreen>;
 
 export default meta;
