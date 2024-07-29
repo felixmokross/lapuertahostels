5b2d5aab42d580b3b9bf505b7e46cae0221a1360
Felix Mokross <felixmokross@gmail.com>
2024-07-29 21:40:20 +0200

    Improve sticky navbar

--- frontend/app/layout/header.tsx
+++ frontend/app/layout/header.tsx
@@ -2,6 +2,7 @@ import { Brand, Common } from "~/payload-types";
 import { Banner } from "./banner";
 import { Navbar } from "./navbar/navbar";
 import { RefObject, useEffect, useRef, useState } from "react";
+import { cn } from "~/common/cn";
 
 type HeaderProps = {
   banner: Common["banner"];
@@ -16,13 +17,14 @@ export function Header({
   allBrands,
   onHeightChanged,
 }: HeaderProps) {
-  const headerRef = useRef<HTMLElement>(null);
+  const headerRef = useRef<HTMLDivElement>(null);
 
   useElementHeightObserver(headerRef, onHeightChanged);
+  const isScrolled = useIsScrolled();
 
   const [isBannerDismissed, setIsBannerDismissed] = useState(false);
   return (
-    <header className="sticky inset-0 z-50 bg-white shadow-lg" ref={headerRef}>
+    <header className="contents">
       {banner?.show && (
         <Banner
           cta={banner.cta?.show ? `${banner.cta.text} →` : undefined}
@@ -33,7 +35,15 @@ export function Header({
           {banner.message!}
         </Banner>
       )}
-      <Navbar brand={brand} allBrands={allBrands} />
+      <div
+        className={cn(
+          "sticky inset-0 z-20 bg-white shadow-lg backdrop-blur-sm transition-colors duration-1000",
+          isScrolled ? "bg-opacity-80" : "bg-opacity-100",
+        )}
+        ref={headerRef}
+      >
+        <Navbar brand={brand} allBrands={allBrands} />
+      </div>
     </header>
   );
 }
@@ -54,4 +64,20 @@ function useElementHeightObserver(
 
     return () => observer.disconnect();
   }, [onHeightChanged, ref]);
+}
+
+const SCROLL_THRESHOULD = 0.3;
+
+function useIsScrolled() {
+  const [scrollY, setScrollY] = useState(0);
+  useEffect(() => {
+    const onScroll = () => setScrollY(window.scrollY);
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  return (
+    typeof window !== "undefined" &&
+    SCROLL_THRESHOULD * window.innerHeight < scrollY
+  );
 }
