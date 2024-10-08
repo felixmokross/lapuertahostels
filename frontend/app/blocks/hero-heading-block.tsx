83eecc5e27a8ef8bf4a2166d54f4c6fde3921cb3
Felix Mokross <felixmokross@gmail.com>
2024-10-08 21:03:58 +0200

    Improve text-only heading (#130)

--- frontend/app/blocks/hero-heading-block.tsx
+++ frontend/app/blocks/hero-heading-block.tsx
@@ -2,15 +2,12 @@ import { Page } from "~/payload-types";
 import { SlideImage } from "./slides-block/slide-image";
 import { OverlayTitle } from "./common/overlay-title";
 import { Heading } from "~/common/heading";
-import { cn } from "~/common/cn";
-import { useTheme } from "~/themes";
 
 type HeroHeadingBlockProps = NonNullable<Page["hero"]>[number] & {
   blockType: "HeroHeading";
 };
 
 export function HeroHeadingBlock({ heading, image }: HeroHeadingBlockProps) {
-  const theme = useTheme();
   return image?.show ? (
     <div className="relative min-h-72 shadow-md md:min-h-96">
       <SlideImage
@@ -26,12 +23,7 @@ export function HeroHeadingBlock({ heading, image }: HeroHeadingBlockProps) {
       />
     </div>
   ) : (
-    <div
-      className={cn(
-        "flex min-h-28 items-center justify-center py-8 text-neutral-700 md:min-h-40",
-        theme.lightBackgroundColor,
-      )}
-    >
+    <div className="flex min-h-28 items-end justify-center py-4 text-neutral-700 md:min-h-40 md:py-8">
       <Heading
         size="medium"
         as="h1"
