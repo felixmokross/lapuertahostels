f04abba68a5a14a24547053ce67b3522adbc1036
Felix Mokross <felixmokross@gmail.com>
2024-10-14 21:10:56 +0200

    Fix Imagekit env variable names

--- cms/src/common/imagekit.ts
+++ cms/src/common/imagekit.ts
@@ -2,8 +2,8 @@ import Imagekit from "imagekit";
 
 export function getImageKit() {
   return new Imagekit({
-    publicKey: process.env.PAYLOAD_PUBLIC_IMAGEKIT_PUBLIC_KEY,
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-    urlEndpoint: process.env.IMAGEKIT_BASE_URL,
+    urlEndpoint: process.env.PAYLOAD_PUBLIC_IMAGEKIT_BASE_URL,
   });
 }
