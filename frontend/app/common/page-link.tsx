402c29e3fbaa2a7416d8e655eff1524bcffe4bcd
Felix Mokross <felixmokross@gmail.com>
2024-10-20 22:28:10 +0200

    Fix bug in PageLink

--- frontend/app/common/page-link.tsx
+++ frontend/app/common/page-link.tsx
@@ -6,14 +6,12 @@ export type PageLinkProps = {
 } & Omit<LinkProps, "to">;
 
 export function PageLink({ link, ...props }: PageLinkProps) {
-  const pageUrl = (link!.page as Page).url;
-
   return (
     <Link
       {...props}
       to={
         link.type === "internal"
-          ? `${pageUrl}${link.queryString ? `?${link.queryString}` : ""}${link.fragment ? `#${link.fragment}` : ""}`
+          ? `${(link.page as Page).url}${link.queryString ? `?${link.queryString}` : ""}${link.fragment ? `#${link.fragment}` : ""}`
           : link.url!
       }
     >
