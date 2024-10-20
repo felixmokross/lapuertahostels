e2b5badccda02345fb47dd855c3991700549c34b
Felix Mokross <felixmokross@gmail.com>
2024-10-20 21:10:02 +0200

    Prepend locale to page links

--- frontend/app/common/page-link.tsx
+++ frontend/app/common/page-link.tsx
@@ -1,17 +1,21 @@
 import { Brand, Page } from "~/payload-types";
 import { Link, LinkProps } from "./link";
+import { useTranslation } from "react-i18next";
 
 export type PageLinkProps = {
   link: NonNullable<Brand["navLinks"]>[number];
 } & Omit<LinkProps, "to">;
 
 export function PageLink({ link, ...props }: PageLinkProps) {
+  const { i18n } = useTranslation();
+  const pageUrl = (link!.page as Page).url;
+
   return (
     <Link
       {...props}
       to={
         link.type === "internal"
-          ? `${(link!.page as Page).url}${link.queryString ? `?${link.queryString}` : ""}${link.fragment ? `#${link.fragment}` : ""}`
+          ? `/${i18n.language}${pageUrl}${link.queryString ? `?${link.queryString}` : ""}${link.fragment ? `#${link.fragment}` : ""}`
           : link.url!
       }
     >
