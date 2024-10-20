231f9d58145578c97c2ed1b2dca8a86a0d073554
Felix Mokross <felixmokross@gmail.com>
2024-10-20 21:18:02 +0200

    Move prepending of locale to more general Link component to make it better available

--- frontend/app/common/link.tsx
+++ frontend/app/common/link.tsx
@@ -2,16 +2,18 @@ import {
   Link as RemixLink,
   LinkProps as RemixLinkProps,
 } from "@remix-run/react";
+import { useTranslation } from "react-i18next";
 
 export type LinkProps = RemixLinkProps;
 
 export function Link({ to, children, ...props }: LinkProps) {
+  const { i18n } = useTranslation();
   const isExternal =
     typeof to === "string" &&
     (to.startsWith("http://") || to.startsWith("https://"));
   return (
     <RemixLink
-      to={to}
+      to={isExternal ? to : `/${i18n.language}${to === "/" ? "" : to}`}
       {...props}
       {...(isExternal ? { target: "_blank", rel: "noreferrer" } : {})}
     >
--- frontend/app/common/page-link.tsx
+++ frontend/app/common/page-link.tsx
@@ -1,13 +1,11 @@
 import { Brand, Page } from "~/payload-types";
 import { Link, LinkProps } from "./link";
-import { useTranslation } from "react-i18next";
 
 export type PageLinkProps = {
   link: NonNullable<Brand["navLinks"]>[number];
 } & Omit<LinkProps, "to">;
 
 export function PageLink({ link, ...props }: PageLinkProps) {
-  const { i18n } = useTranslation();
   const pageUrl = (link!.page as Page).url;
 
   return (
@@ -15,7 +13,7 @@ export function PageLink({ link, ...props }: PageLinkProps) {
       {...props}
       to={
         link.type === "internal"
-          ? `/${i18n.language}${pageUrl}${link.queryString ? `?${link.queryString}` : ""}${link.fragment ? `#${link.fragment}` : ""}`
+          ? `${pageUrl}${link.queryString ? `?${link.queryString}` : ""}${link.fragment ? `#${link.fragment}` : ""}`
           : link.url!
       }
     >
