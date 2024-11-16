3a6d1c85f8b75ed52c9614a9dc7ce699f7c97d12
Felix Mokross <felixmokross@gmail.com>
2024-11-16 09:27:59 +0100

    Move preview bar to top, make it more responsive, non-sticky

--- frontend/app/layout/preview-bar.stories.tsx
+++ frontend/app/layout/preview-bar.stories.tsx
@@ -8,14 +8,6 @@ const meta = {
   parameters: {
     layout: "fullscreen",
   },
-  decorators: [
-    (Story) => (
-      <div className="flex h-screen items-center justify-center">
-        <p className="text-sm text-neutral-600">Content placeholder</p>
-        <Story />
-      </div>
-    ),
-  ],
 } satisfies Meta<typeof PreviewBar>;
 
 export default meta;
@@ -23,7 +15,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    className: "absolute bottom-0 inset-x-0",
     adminLocale: "en",
   },
 };
