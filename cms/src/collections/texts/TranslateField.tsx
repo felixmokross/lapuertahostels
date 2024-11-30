540a47102c4307b5977f01eea07284e3b9d2bd0d
Felix Mokross <felixmokross@gmail.com>
2024-11-30 21:50:22 +0100

    Fix translate drawer for empty texts (#202)
    * Fix translate drawer for empty texts
    
    * Fix isLongContent return value
    
    * Fix remaining type error

--- cms/src/collections/texts/TranslateField.tsx
+++ cms/src/collections/texts/TranslateField.tsx
@@ -307,8 +307,8 @@ function TableContentCell({
 }
 
 type AllLocalesText = Pick<Text, "type"> & {
-  text?: Record<string, string>;
-  richText_html?: Record<string, string>;
+  text?: Record<string, string | undefined>;
+  richText_html?: Record<string, string | undefined>;
 };
 
 type AllLocalesTextRendererProps = {
@@ -326,7 +326,7 @@ function AllLocalesTextRenderer({
       {data.type === "richText" && data.richText_html && (
         <div
           dangerouslySetInnerHTML={{
-            __html: data.richText_html[localeCode],
+            __html: data.richText_html[localeCode] ?? "",
           }}
           lang={localeCode}
           className="rich-text-html tw-prose-xl tw-pointer-events-none tw-hyphens-auto tw-font-serif"
@@ -345,5 +345,5 @@ function isLongContent(data: AllLocalesText, localeCode: string) {
       : data.richText_html
         ? data.richText_html[localeCode]
         : "";
-  return fullText.length > 200;
+  return !!fullText && fullText.length > 200;
 }
