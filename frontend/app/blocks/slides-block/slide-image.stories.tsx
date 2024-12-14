aab0a509bd85b62c3dcd05fe26f14e7fb63dc86d
Felix Mokross <felixmokross@gmail.com>
2024-12-14 16:02:19 +0100

    Fix unrelated story

--- frontend/app/blocks/slides-block/slide-image.stories.tsx
+++ frontend/app/blocks/slides-block/slide-image.stories.tsx
@@ -17,6 +17,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    media: media("/azzedine-rouichi-gc5OYAll-rc-unsplash.jpg", "Example image"),
+    media: media("azzedine-rouichi-gc5OYAll-rc-unsplash.jpg", "Example image"),
   },
 };
