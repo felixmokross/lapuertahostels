4c319aecf41640fbc385d64ae9d327c8f0d77eb5
Felix Mokross <felixmokross@gmail.com>
2024-12-21 21:46:46 +0100

    Create brand if it does not exist

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -2,6 +2,25 @@ import { test, expect } from "@playwright/test";
 import { createId } from "@paralleldrive/cuid2";
 
 test("has title", async ({ page }) => {
+  const puertaBrand = await cmsGet("brands/puerta");
+  if (!puertaBrand) {
+    const media = await cmsCreate("media", {
+      filename: "logo-puerta-simple.png",
+      mimeType: "image/png",
+    });
+    const homeLink = await cmsCreate("links", {
+      type: "external",
+      url: "/",
+    });
+    await cmsCreate("brands", {
+      id: "puerta",
+      name: "La Puerta Hostels",
+      logo: media.id,
+      footer: { linkGroups: [] },
+      homeLink: homeLink.id,
+    });
+  }
+
   const titleText = await cmsCreate("texts", {
     type: "plainText",
     text: "E2E Test Page",
@@ -46,4 +65,25 @@ async function cmsCreate(collection: string, content: object) {
   }
 
   return (await result.json()).doc;
+}
+
+async function cmsGet(collection: string) {
+  const result = await fetch(
+    new URL(`/api/${collection}?pagination=false`, process.env.CMS_BASE_URL),
+    {
+      method: "GET",
+      headers: {
+        Authorization: `users API-Key ${process.env.CMS_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+    },
+  );
+
+  if (!result.ok) {
+    if (result.status === 404) return null;
+
+    throw new Error(`Failed to create test page: ${result.status}`);
+  }
+
+  return (await result.json()).docs;
 }
