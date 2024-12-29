dc944a69f1f045bcfc78f3da18380ff5ec421e42
Felix Mokross <felixmokross@gmail.com>
2024-12-29 12:15:56 +0100

    Db migration

--- cms/src/migrations/index.ts
+++ cms/src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20241118_165700_migration from "./20241118_165700_migratio
 import * as migration_20241118_173327_migration from "./20241118_173327_migration";
 import * as migration_20241119_204317_migration from "./20241119_204317_migration";
 import * as migration_20241207_173838_migrate_media_alt_to_texts from "./20241207_173838_migrate_media_alt_to_texts";
+import * as migration_20241229_110109_rename_pages_collection from "./20241229_110109_rename_pages_collection";
 
 export const migrations = [
   {
@@ -23,5 +24,10 @@ export const migrations = [
     up: migration_20241207_173838_migrate_media_alt_to_texts.up,
     down: migration_20241207_173838_migrate_media_alt_to_texts.down,
     name: "20241207_173838_migrate_media_alt_to_texts",
+  },
+  {
+    up: migration_20241229_110109_rename_pages_collection.up,
+    down: migration_20241229_110109_rename_pages_collection.down,
+    name: "20241229_110109_rename_pages_collection",
   },
 ];
