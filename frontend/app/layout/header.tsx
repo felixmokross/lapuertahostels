30afcd9738853ae579a912715dcae82858ee78dd
Felix Mokross <felixmokross@gmail.com>
2024-12-30 09:32:50 +0100

    feat(frontend): Overhaul navbar UX

--- frontend/app/layout/header.tsx
+++ frontend/app/layout/header.tsx
@@ -1,7 +1,6 @@
 import { Brand } from "~/payload-types";
 import { Banner } from "./banner";
 import { Navbar, NavbarProps } from "./navbar/navbar";
-import { RefObject, useEffect, useRef, useState } from "react";
 
 type HeaderProps = {
   hasSession?: boolean;
@@ -10,9 +9,6 @@ type HeaderProps = {
 } & Pick<NavbarProps, "onHeightChanged">;
 
 export function Header({ brand, allBrands, onHeightChanged }: HeaderProps) {
-  const navbarRef = useRef<HTMLDivElement>(null);
-  const isScrolled = useIsScrolled(navbarRef);
-
   if (brand.banner != null && typeof brand.banner !== "object") {
     throw new Error("Brand banner is not an object");
   }
@@ -23,26 +19,7 @@ export function Header({ brand, allBrands, onHeightChanged }: HeaderProps) {
         brand={brand}
         allBrands={allBrands}
         onHeightChanged={onHeightChanged}
-        isScrolled={isScrolled}
-        ref={navbarRef}
       />
     </header>
   );
-}
-
-function useIsScrolled(ref: RefObject<HTMLElement>): boolean {
-  const [isScrolled, setIsScrolled] = useState(false);
-  useEffect(() => {
-    const onScroll = () => {
-      if (!ref.current) return;
-
-      setIsScrolled(ref.current.getBoundingClientRect().top === 0);
-    };
-
-    onScroll();
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
-  }, [ref]);
-
-  return isScrolled;
 }
