f81bcb0138a4119e9405c31d2c988ddea6936e02
Felix Mokross <felixmokross@gmail.com>
2025-01-03 02:48:41 +0100

    Further migration steps towards RR7

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,30 +1,12 @@
 import { reactRouter } from "@react-router/dev/vite";
-import { installGlobals } from "react-router";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
-installGlobals();
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [
-    !isVitest() &&
-      !isStorybook() &&
-      reactRouter({
-        ignoredRouteFiles: ["**/.*"],
-        future: {
-          v3_fetcherPersist: true,
-          v3_relativeSplatPath: true,
-          v3_throwAbortReason: true,
-          v3_singleFetch: true,
-          v3_lazyRouteDiscovery: true,
-          v3_routeConfig: true,
-        },
-      }),
-    tsconfigPaths(),
-  ],
+  plugins: [!isVitest() && !isStorybook() && reactRouter(), tsconfigPaths()],
   test: {
     environment: "jsdom",
     setupFiles: "./tests/setup.ts",
