33dd2257eb7c60ce71e1ea924af1106fe0d36970
Felix Mokross <felixmokross@gmail.com>
2025-04-06 15:01:00 +0200

    Make redirect e2e tests independent of base URL

--- tests/e2e/tests/redirects.spec.ts
+++ tests/e2e/tests/redirects.spec.ts
@@ -1,26 +1,26 @@
 import test, { expect } from "@playwright/test";
 import { createPage } from "../common/cms";
 
-test("redirects to fallback locale", async ({ page, baseURL }) => {
+test("redirects to fallback locale", async ({ page }) => {
   const testPage = await createPage();
 
   await page.goto(testPage.pathname);
 
-  await expect(page).toHaveURL(`${baseURL}/en${testPage.pathname}`);
+  expect(page.url().endsWith(`/en${testPage.pathname}`)).toBeTruthy();
 });
 
-test("redirects to request locale", async ({ page, baseURL }) => {
+test("redirects to request locale", async ({ page }) => {
   const testPage = await createPage();
 
   await page.goto(testPage.pathname + "?lng=es");
 
-  await expect(page).toHaveURL(`${baseURL}/es${testPage.pathname}?lng=es`);
+  expect(page.url().endsWith(`/es${testPage.pathname}?lng=es`)).toBeTruthy();
 });
 
-test("redirects to path without trailing slash", async ({ page, baseURL }) => {
+test("redirects to path without trailing slash", async ({ page }) => {
   const testPage = await createPage();
 
   await page.goto(testPage.pathname + "/");
 
-  await expect(page).toHaveURL(`${baseURL}/en${testPage.pathname}`);
+  expect(page.url().endsWith(testPage.pathname)).toBeTruthy();
 });
