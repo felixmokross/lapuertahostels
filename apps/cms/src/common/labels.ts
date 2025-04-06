204584ea81b0f686a1c505b63f4543a750fb548f
Felix Mokross <felixmokross@gmail.com>
2025-04-06 23:12:09 +0200

    Fix getLabelText function after Payload update

--- apps/cms/src/common/labels.ts
+++ apps/cms/src/common/labels.ts
@@ -21,7 +21,7 @@ export function getLabelText(data: LabelData, i18n: I18nClient) {
   }
 
   if (typeof data === "function") {
-    return data({ t: i18n.t as Parameters<LabelFunction>[0]["t"] });
+    return data({ t: i18n.t as Parameters<LabelFunction>[0]["t"], i18n });
   }
 
   throw new Error("Invalid label type");
