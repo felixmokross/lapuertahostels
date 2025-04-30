ff0ad74e7529900999c411f84f474c2898820929
Felix Mokross <felixmokross@gmail.com>
2025-04-30 21:23:34 +0200

    fix(cms): Show page pathname in current locale in Redirects list view (#351)

--- apps/cms/src/collections/redirects/to-cell.tsx
+++ apps/cms/src/collections/redirects/to-cell.tsx
@@ -1,12 +1,19 @@
-import { DefaultServerCellComponentProps } from "payload";
+"use client";
 
-export async function ToCell({
-  cellData,
-  payload,
-}: DefaultServerCellComponentProps) {
-  const page = await payload.findByID({
-    collection: "pages",
-    id: cellData.page,
-  });
-  return `${page.pathname}${cellData.queryString ? `?${cellData.queryString}` : ""}${cellData.fragment ? `#${cellData.fragment}` : ""}`;
+import { TranslationsKey, TranslationsObject } from "@/translations";
+import { usePayloadAPI, useTranslation } from "@payloadcms/ui";
+import { DefaultCellComponentProps } from "payload";
+
+export function ToCell({ cellData }: DefaultCellComponentProps) {
+  const [{ data: page, isLoading, isError }] = usePayloadAPI(
+    `/api/pages/${encodeURIComponent(cellData.page)}`,
+  );
+  const { t } = useTranslation<TranslationsObject, TranslationsKey>();
+
+  if (isLoading) return <div>{t("general:loading")}…</div>;
+  if (isError) {
+    return <div>{t("error:loadingDocument", { id: cellData.page })}</div>;
+  }
+
+  return `${t("custom:pages:name")}: ${page.pathname}${cellData.queryString ? `?${cellData.queryString}` : ""}${cellData.fragment ? `#${cellData.fragment}` : ""}`;
 }
