228221099df1515b521ec9925c024856d3c62e69
Felix Mokross <felixmokross@gmail.com>
2025-05-02 13:16:17 -0500

    Remove old, incompatible migrations

--- apps/cms/src/migrations/20250420_151225_recreate_pages_indexes.ts
+++
@@ -1,9 +0,0 @@
-import { MigrateDownArgs, MigrateUpArgs } from "@payloadcms/db-mongodb";
-
-export async function up({ payload }: MigrateUpArgs): Promise<void> {
-  await payload.db.collections.pages.base.syncIndexes();
-}
-
-export async function down(_: MigrateDownArgs): Promise<void> {
-  // Migration code
-}
